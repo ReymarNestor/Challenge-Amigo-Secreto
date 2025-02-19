62729d604dcb534eb748ece12354ba0cdb613af9
Nestor Reyes <marn95mr@gmail.com>
2025-02-18 23:56:27 -0600

    Primer commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Declaración de variable que guaradara el nombre de los amigos
+let listaAmigos = [];
+
+//Función que agrega amigos a la lista
+
+function agregarAmigo () {
+    let nombreAgregado = document.querySelector
+}
